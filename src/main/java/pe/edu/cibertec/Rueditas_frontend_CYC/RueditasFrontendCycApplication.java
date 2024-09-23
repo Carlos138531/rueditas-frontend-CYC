31a470f2ded6dee569d72d13753eb7fb4f773784
Carlos Yaringaño <carlos138531@gmail.com>
2024-09-22 23:39:26 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/Rueditas_frontend_CYC/RueditasFrontendCycApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Rueditas_frontend_CYC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontendCycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontendCycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Rueditas_frontend_CYC/RueditasFrontendCycApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Rueditas_frontend_CYC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasFrontendCycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
